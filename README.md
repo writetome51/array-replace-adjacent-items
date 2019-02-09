2480aa6d938c6562b9759c1b4e250707293efe40
Steve Thompson <writetome51@gmail.com>
2019-02-08 23:49:25 -0700

    minor refactoring

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
-Use this function as a more reliable alternative to Array.splice() when only replacing items in the array.
-If numItemsToReplace is less than 1, it triggers error.
-startingIndex can be negative or positive.
+# _replaceAdjacentItems()
 
- _replaceAdjacentItems(startingIndex, numItemsToReplace, newValues: any[], arrayToModify); // returns void.
+####  _replaceAdjacentItems(startingIndex, numItemsToReplace, newValues: any[], array): void
+
+Replaces adjacent items in <b>array</b>.  
+Replaces total of <b>numItemsToReplace,</b> beginning at <b>startingIndex</b>, with <b>newValues</b>.  
+Use this function as a more reliable alternative to Array.splice() when only replacing items in the 
+array.  
+<b>startingIndex</b> can be negative or positive.
+
+## Installation
+
+You must have npm installed first.  Then, in the command line:
+
+```bash
+npm install @writetome51/array-replace-adjacent-items
+```
+
+## Loading
+```
+// If using TypeScript:
+import {  _replaceAdjacentItems } from '@writetome51/array-replace-adjacent-items';
+// If using ES5 JavaScript:
+var  _replaceAdjacentItems = 
+    require('@writetome51/array-replace-adjacent-items')._replaceAdjacentItems;
+```
--- _replaceAdjacentItems.js
+++ _replaceAdjacentItems.js
@@ -2,20 +2,19 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var errorIfNotInteger_1 = require("basic-data-handling/errorIfNotInteger");
 var errorIfNotArray_1 = require("basic-data-handling/errorIfNotArray");
-var ifIndexValid_getActionResult_1 = require("@writetome51/array-and-index-validation/ifIndexValid_getActionResult");
 var errorIfArrayTooShortToMeetAdjacentItemsRequest_1 = require("@writetome51/array-and-index-validation/errorIf/errorIfArrayTooShortToMeetAdjacentItemsRequest");
+var errorIfIndexNotValid_1 = require("@writetome51/array-and-index-validation/errorIf/errorIfIndexNotValid");
 // Use this function as a more reliable alternative to Array.splice() when only
 // replacing items in the array. If numItemsToReplace is less than 1, it triggers error.
 // startingIndex can be negative or positive.
 function _replaceAdjacentItems(startingIndex, numItemsToReplace, newValues, arrayToModify) {
-    ifIndexValid_getActionResult_1.ifIndexValid_getActionResult(startingIndex, function () {
-        errorIfNotInteger_1.errorIfNotInteger(numItemsToReplace);
-        errorIfNotArray_1.errorIfNotArray(newValues);
-        errorIfArrayTooShortToMeetAdjacentItemsRequest_1.errorIfArrayTooShortToMeetAdjacentItemsRequest(startingIndex, numItemsToReplace, arrayToModify);
-        if (numItemsToReplace > 0)
-            arrayToModify.splice.apply(arrayToModify, [startingIndex, numItemsToReplace].concat(newValues));
-        else
-            throw new Error('The number of items to replace must be greater than zero');
-    }, arrayToModify);
+    errorIfIndexNotValid_1.errorIfIndexNotValid(startingIndex, arrayToModify);
+    errorIfNotInteger_1.errorIfNotInteger(numItemsToReplace);
+    errorIfNotArray_1.errorIfNotArray(newValues);
+    errorIfArrayTooShortToMeetAdjacentItemsRequest_1.errorIfArrayTooShortToMeetAdjacentItemsRequest(startingIndex, numItemsToReplace, arrayToModify);
+    if (numItemsToReplace > 0)
+        arrayToModify.splice.apply(arrayToModify, [startingIndex, numItemsToReplace].concat(newValues));
+    else
+        throw new Error('The number of items to replace must be greater than zero');
 }
 exports._replaceAdjacentItems = _replaceAdjacentItems;
